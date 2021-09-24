67dd496686fd0a9189bb912c34ddb984490d9095
ahmedrabidev <91339251+ahmedrabidev@users.noreply.github.com>
2021-09-25 01:56:33 +0200

    Update services.php

--- config/services.php
+++ config/services.php
@@ -26,12 +26,12 @@ return [
     'facebook' => [
         'client_id' => env('FACEBOOK_CLIENT_ID'),
         'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
-        'redirect' => 'http://localhost/readyproject/ar/login/facebook/callback',
+        'redirect' => '',
     ],
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'http://localhost/readyproject/ar/login/google/callback',
+        'redirect' => '',
     ],
 
 ];
